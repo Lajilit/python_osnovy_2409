c0fd2e7657393e3e68c25be50141ede4722696d9
Lajilit <lajilit@gmail.com>
2020-10-04 00:06:56 +0300

    little error correction

--- lesson_2_homework_2.py
+++ lesson_2_homework_2.py
@@ -7,19 +7,19 @@
 def list_element_exchange(my_list):
     length = len(my_list)
     if length == 0:
-        print('Список пуст')
+        return 'Список пуст'
     elif length == 1:
-        print(my_list)
+        return my_list
     else:
         length = length if length % 2 == 0 else length - 1
         for i in range(length):
             if i % 2 == 0:
                 my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]
-        print(my_list)
+        return my_list
 
 
 if __name__ == '__main__':
-    list_element_exchange([1, 2, 3, 4, 5, 6, 7, 8])
-    list_element_exchange([1, 2, 3, 4, 5, 6, 7])
-    list_element_exchange([1])
-    list_element_exchange([])
+    print(list_element_exchange([1, 2, 3, 4, 5, 6, 7, 8]))
+    print(list_element_exchange([1, 2, 3, 4, 5, 6, 7]))
+    print(list_element_exchange([1]))
+    print(list_element_exchange([]))
