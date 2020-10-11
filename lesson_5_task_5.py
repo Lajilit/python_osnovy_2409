e398d3c3619692431b399ca2203a3ddabbed54a2
Lajilit <lajilit@gmail.com>
2020-10-11 15:51:34 +0300

    task 5

--- lesson_5_task_5.py
+++ lesson_5_task_5.py
@@ -1,9 +1,11 @@
 # 5. Создать (программно) текстовый файл, записать в него программно
 # набор чисел, разделенных пробелами. Программа должна подсчитывать
 # сумму чисел в файле и выводить ее на экран.
+from random import randint
 
 with open('l5t5_text.txt', 'w', encoding='UTF-8') as numbers_write:
-    numbers_write.write('1 2 3 4 5 6 7 8 9 0')
+    numbers_list = [str(randint(0, 100)) for num in range(20)]
+    numbers_write.write(' '.join(numbers_list))
 
 with open('l5t5_text.txt', 'r', encoding='UTF-8') as numbers_read:
     numbers = []
